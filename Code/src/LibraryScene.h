805a58a9349b9328ea7f00c82edf1811dd1bb97d
Menno <my.bil@student.avans.nl>
2022-03-11 22:55:27 +0100

    [ADDED] test scene

--- Code/src/LibraryScene.h
+++ Code/src/LibraryScene.h
@@ -1,17 +1,48 @@
 #pragma once
 #include "Scene.h"
+#include "input/Input.h"
 #include "output/Graphics.h"
+#include "output/DotMatrix.h"
 
-class LibraryScene : public Scene
+class LibraryScene : public Scene, EventListener
 {
+private:
+    Vector2 pos;
+
 public:
+    LibraryScene()
+    {
+        Input::AddListener(this);
+
+        pos = {0, 0};
+    }
+
+    ~LibraryScene()
+    {
+        Input::RemoveListener(this);
+    }
+
     void update(float delta) override
     {
-
+        delay(50);
     }
 
     void draw() override
     {
-        Graphics::GetDotMatrix()->clear();
+        DotMatrix* matrix = Graphics::GetDotMatrix();
+        
+        matrix->clear();
+        matrix->setPixel(pos.x, pos.y, true);
+    }
+
+    void onEvent(const Event& event) override
+    {
+        switch (event.type)
+        {
+            case EventType::JoyStickUp: pos.y++; break;
+            case EventType::JoyStickDown: pos.y--; break;
+            case EventType::JoyStickLeft: pos.x--; break;
+            case EventType::JoyStickRight: pos.x++; break;
+        }
     }
 };
